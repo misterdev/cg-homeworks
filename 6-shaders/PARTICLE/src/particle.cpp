b96f8cbc94572ca456257db2c668b95647a08274
Devid Farinelli <devid.farinelli@gmail.com>
2017-05-12 18:28:36 +0200

    [ENDED] 6-particle, manca glPointSize

--- 6-shaders/PARTICLE/src/particle.cpp
+++ 6-shaders/PARTICLE/src/particle.cpp
@@ -6,49 +6,46 @@ GLfloat velocity[N][3];
 GLubyte color[N][3];
 
 GLuint program;
-GLuint vxParam, vyParam, timeParam;
+GLuint vxParam, vyParam, vzParam, timeParam;
 
 float timeStart = 0.0;
 
-void randomizeParticles()
-{
+void randomizeParticles() {
     int i, j;
-    for(i=0;i<N;i++)
-    {
+    for(i=0;i<N;i++) {
         velocity[i][0] = 0.001 * 0.5 * ((rand()%256)/256.0-0.5);
         velocity[i][1] = 0.001 * 1.0 * ((rand()%256)/256.0+2.0);
+        velocity[i][2] = 0.001 * 0.5 * ((rand()%256)/256.0-0.5);
     }
 
-    for(i=0;i<N;i++)
-    {
+    for(i=0;i<N;i++) {
         for(j=0; j<3; j++)
             color[i][j] = rand()%256;
     }
 }
 
-static void init()
-{
+static void init() {
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     glColor3f(0.0,0.0,0.0);
-    glPointSize(10.0);
+    // glPointSize(40.0);
 
     glDisable(GL_DEPTH_TEST);
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    //gluOrtho2D(-1.0,1.0,-1.0,1.0);
+    // gluOrtho2D(-1.0,1.0,-1.0,1.0);
     gluPerspective(85.0, (double) glutGet(GLUT_WINDOW_WIDTH) / (double) glutGet(GLUT_WINDOW_HEIGHT), 1.0, 20.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt (2.0, 2.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    gluLookAt (1.0, 2.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
     glEnable(GL_DEPTH_TEST);
 
     // enable changing the point size in the vertex shader via the gl_PointSize variable;
-    //glEnable(GL_POINT_SPRITE);
-    //glEnable(GL_VERTEX_PROGRAM_POINT_SIZE);
+    glEnable(GL_POINT_SPRITE);
+    glEnable(GL_VERTEX_PROGRAM_POINT_SIZE);
 
-   #ifndef __APPLE__
+    #ifndef __APPLE__
         glewInit();
     #endif
 
@@ -57,27 +54,26 @@ static void init()
     timeParam = glGetUniformLocation(program, "time");
     vxParam = glGetAttribLocation(program, "vx");
     vyParam = glGetAttribLocation(program, "vy");
+    vzParam = glGetAttribLocation(program, "vz");
     
-
     srand(time(0));
 
     randomizeParticles();
 }
 
-static void draw(void)
-{
+static void draw(void) {
     int i;
     float time = glutGet(GLUT_ELAPSED_TIME) - timeStart;
     glUniform1f(timeParam, time);
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    double h=0.05;
+    double h = 0.05;
     glBegin(GL_QUADS);
-    for(i=0; i<N; i++)
-    {
+    for(i=0; i<N; i++) {
         glColor3ubv(color[i]);
         glVertexAttrib1f(vxParam, velocity[i][0]);
         glVertexAttrib1f(vyParam, velocity[i][1]);
+        glVertexAttrib1f(vzParam, velocity[i][2]);
 
         glVertex2d(h, h);
         glVertex2d(h, -h);
@@ -90,18 +86,15 @@ static void draw(void)
     glutPostRedisplay();
 }
 
-void keyboard(unsigned char key, int x, int y)
-{
+void keyboard(unsigned char key, int x, int y) {
     commonKeyboard(key, x, y);
-    if(key == ' ')
-    {
+    if(key == ' ') {
         timeStart = glutGet(GLUT_ELAPSED_TIME);
         randomizeParticles();
     }
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
     glutInitWindowSize(512, 512);
--- 6-shaders/PARTICLE/src/v.glsl
+++ 6-shaders/PARTICLE/src/v.glsl
@@ -1,17 +1,15 @@
 uniform float time;
-attribute  float vx, vy, vz; //add velocity in z-direction
+attribute  float vx, vy, vz;
 const float a = -0.000001;
 
-void main()
-{
+void main() {
     
     vec4 t = gl_Vertex;
     t.y = gl_Vertex.y + vy*time + 0.5*a*time*time;
-    t.x = gl_Vertex.x + vx*time;
-	// utilize t.z and gl_PointSize
-	//FATTO t.z = gl_Vertex.x + vz*time
-
-	//FATTO gl_PointSize = max(t.y * 50.0, 1.0)
+    t.x = gl_Vertex.x + vx*2.5*time;
+    t.z = gl_Vertex.z + vz*2.5*time;
+	// gl_PointSize = gl_Vertex.y * 10.0;
+	//TODO varia dimensione in base alla y
 
     gl_Position = gl_ModelViewProjectionMatrix*t;
     gl_FrontColor =  gl_Color;
