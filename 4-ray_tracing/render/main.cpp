86e7cd661de98729c7cd56b5905f03edeb266282
Devid Farinelli <devid.farinelli@gmail.com>
2018-11-16 22:37:20 +0100

    [4 RAY TRACING] first commit

---
+++ 4-ray_tracing/render/main.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#if defined(__APPLE__) || defined(MACOSX)
+#include <GLUT/glut.h>
+#else
+#include <GL/freeglut.h>
+#endif
+
+#include "argparser.h"
+#include "glCanvas.h"
+
+#include "mesh.h"
+#include "radiosity.h"
+#include "raytracer.h"
+
+// =========================================
+// =========================================
+
+int
+main (int argc, char *argv[])
+{
+  glutInit (&argc, argv);
+
+  srand(0);
+  ArgParser *args = new ArgParser (argc, argv);
+
+  Mesh *mesh = new Mesh ();
+  mesh->Load (args->input_file, args);
+  RayTracer *raytracer = new RayTracer (mesh, args);
+  Radiosity *radiosity = new Radiosity (mesh, args);
+  raytracer->setRadiosity (radiosity);
+  radiosity->setRayTracer (raytracer);
+
+  GLCanvas glcanvas;
+  glcanvas.initialize (args, mesh, raytracer, radiosity);
+
+  // well it never returns from the GLCanvas loop...
+  delete args;
+  return 0;
+}
+
+// =========================================
+// =========================================
